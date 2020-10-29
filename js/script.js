1007b8dd56b68c9f3e54b1e57b4c97b72630ed44
l7ana <59889633+l7ana@users.noreply.github.com>
2020-10-29 10:17:09 -1000

    test

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Overlay Nav Menu
+const mymenubutton = document.querySelector('.menu-button');
+const mysitenav = document.querySelector('.site-header .site-nav');
+
+mymenubutton.onclick = function () {
+  if ( mysitenav.getAttribute('data-navstate') ==='open') {
+    mysitenav.setAttribute('data-navstate', 'closed');
+  } else {
+      mysitenav.setAttribute('data-navstate', 'open');
+    }
+  }
