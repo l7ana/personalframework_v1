c2e2f5e0b00fd238c4e574dd1eced1ac1ed77b95
l7ana <59889633+l7ana@users.noreply.github.com>
2020-11-05 12:06:59 -1000

    step 3

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ mymenubutton.onclick = function () {
       mysitenav.setAttribute('data-navstate', 'open');
     }
   }
+
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+    const myobserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.setAttribute('data-sectionstate', 'active');
+          } else {
+              entry.target.setAttribute('data-sectionstate', 'inactive');
+          }
+      });
+  });
+  document.querySelectorAll('.animate-on-scroll').forEach( (section) => {
+      myobserver.observe(section);
+  });
